5aac64e8626102957e8c6f30969d74deb632750b
LavanyaCS <lavanyachandranece@gmail.com>
2025-05-21 12:27:35 +0530

    todo-list

---
+++ js/script.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded',() => {
+const taskInput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const taskList = document.getElementById("task-list");
+const emptyImage = document.querySelector(".empty-image");
+const todosContainer = document.querySelector(".todos-container");
+
+const toggleEmptySpace = () => {
+    emptyImage.style.display = taskList.children.length === 0 ? 'block' : 'none';
+    todosContainer.style.width = taskList.children.length > 0 ? '100%' : '50%';
+};
+const addTask = (text,completed = false) => {
+   
+    const taskText = text || taskInput.value.trim();
+    if(!taskText){
+        return;
+    }
+    const li = document.createElement('li');
+    li.innerHTML  = `<input type="checkbox" class="checkbox" ${completed ? 'checked' : ''} />
+    <span class="ms-3">${taskText}</span>
+    <div class="task-buttons">
+    <button class="btn btn-warning text-white edit-btn"><i class="fas fa-pen"></i></button>
+    <button class="btn btn-delete delete-btn"><i class="fas fa-trash"></i></button>`;
+  
+    const checkbox = li.querySelector('.checkbox');
+    const editBtn = li.querySelector('.edit-btn');
+    if(completed){
+        li.classList.add('completed');
+        editBtn.disabled = true;
+        editBtn.style.opacity = '0.5';
+        editBtn.style.pointerEvents = 'none';
+    }
+    checkbox.addEventListener('change',() =>{
+        const isChecked = checkbox.checked;
+        li.classList.toggle('completed',isChecked);
+        editBtn.disabled = isChecked;
+        editBtn.style.opacity = isChecked ? '0.5':'1';
+        editBtn.style.pointerEvents = isChecked ? 'none' : 'auto';
+    })
+editBtn.addEventListener('click',() =>{
+    if(!checkbox.checked){
+        taskInput.value = li.querySelector('span').textContent;
+        li.remove();
+    toggleEmptySpace();     }
+})
+
+  li.querySelector('.delete-btn').addEventListener('click',() => {
+    li.remove();
+    toggleEmptySpace();
+  });
+    taskList.appendChild(li);
+    taskInput.value = '';
+    toggleEmptySpace();
+};
+addTaskBtn.addEventListener('click',() => addTask());
+taskInput.addEventListener('keypress',(e) => {
+if(e.key === 'Enter'){
+    e.preventDefault();
+    addTask();
+}
+});
+})
